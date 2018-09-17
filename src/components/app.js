2891746bb285b5a7023608d4aa6a17675b5028c9
Nicholas Evans <nevans85.pro@gmail.com>
2018-09-17 16:01:05 -0600

    write basic landing page and app.js file

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import logo from "./logo.svg";
+import Navigation from "./navigation/navigationContainer";
+import AboutMe from "./mainContent/aboutMe/aboutMeContainer";
+import Contact from "./mainContent/contact/contactContainer";
+import Projects from "./mainContent/projects/projectsContainer";
+import Landing from "./mainContent/landing/landing";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      
+      <div className="root_body">
+        <header>
+          <Route path="/" component={Navigation} />
+        </header>
+      </div>
     );
   }
 }
