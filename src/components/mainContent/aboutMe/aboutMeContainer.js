552ecce7f674179704726b48b27a2133f7bbd112
Nicholas Evans <nevans85.pro@gmail.com>
2018-09-17 15:05:18 -0600

    add base components and restructure

---
+++ src/components/mainContent/aboutMe/aboutMeContainer.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux";
+import AboutMe from "component_path";
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AboutMe);
