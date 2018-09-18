b2c0e4b7428eda4854ea22530bfea1eee038d9fe
Nicholas Evans <nevans85.pro@gmail.com>
2018-09-17 21:40:59 -0600

    implement redux elements to utiliZe store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Root from "./components/root";
+import configureStore from "./store";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  let store = {};
+  //  if (window.currentUser) {
+  //    const preloadedState = { session: { currentUser: window.currentUser } };
+  //    store = configureStore(preloadedState);
+  //    delete window.currentUser;
+  //  } else {
+  //    store = configureStore();
+  //  }
+  ReactDOM.render(<Root store={store} />, root);
+});
 registerServiceWorker();
