9a9603f19c7ff75bc24b9dd1fcb71729456fc002
Nicholas Evans <nevans85.pro@gmail.com>
2018-09-19 15:09:35 -0600

    move content files

---
+++ src/Content/projectsContent.js
@@ -0,0 +1,14 @@
+const ProjectsContent = {
+  1: {
+    id: 1,
+    name: "",
+    shortDescription: "",
+    longDescription: "",
+    liveLink: "",
+    repoLink: "",
+    iconImage: "",
+    previewImage: ""
+  }
+};
+
+export default ProjectsContent;
