343d85fd4328e1545a35c2885939edbb0421370e
Nicholas Evans <nevans85.pro@gmail.com>
2018-10-23 13:14:55 -0600

    write basic contact form

--- src/components/mainContent/contact/contact.jsx
+++ src/components/mainContent/contact/contact.jsx
@@ -3,12 +3,56 @@ import React from "react";
 class Contact extends React.Component {
   constructor(props) {
     super(props);
+    this.state = this.defaultState();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  defaultState() {
+    return {
+      name: "",
+      email: "",
+      message: ""
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  updateInput(key) {
+    return e => {
+      this.setState({ [key]: e.target.value });
+    };
   }
 
   render() {
     return (
       <div>
-        <p> This is the page for the contact form</p>
+        <form className="contactForm" onSubmit={this.handleSubmit}>
+          <label>Your Name: </label>
+          <input
+            onChange={this.updateInput("name")}
+            type="text"
+            value={this.state.name}
+          />
+
+          <label>Your Email: </label>
+          <input
+            onChange={this.updateInput("email")}
+            type="text"
+            value={this.state.email}
+          />
+
+          <label>Message: </label>
+          <textarea
+            onChange={this.updateInput("message")}
+            rows="10"
+            value={this.state.message}
+          />
+
+          <input className="contactFormSubmit" type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
